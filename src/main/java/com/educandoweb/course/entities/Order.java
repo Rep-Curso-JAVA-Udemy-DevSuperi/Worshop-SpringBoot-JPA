e5399ea38ca1c2f87efc2b847b1d276d26e70f70
Mateus Bonfanti <matebonfanti@gmail.com>
2024-03-14 11:10:33 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -118,7 +118,15 @@ public class Order implements Serializable{
 		this.payment = payment;
 	}
 
-
+	public Double getTotal() {
+		double sum = 0;
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
+	
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
